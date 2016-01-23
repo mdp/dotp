2ededecbecc306e1b77c3c8de03df0844cc6fb48
mdp <m@mdp.im>
2016-01-22 16:14:46 -0800

    Simpify demo

--- demo/src/app/components/Challenger.jsx
+++ demo/src/app/components/Challenger.jsx
@@ -11,11 +11,6 @@ const containerStyle = {
 
 // This would normally all by done on the server, but for demonstration
 // purposes we are doing it all client side
-const ServerSecret = new Buffer('This is the seed for the server secret key')
-const ServerPrivateKey = dotpCrypt.nacl.hash(ServerSecret).slice(0,32)
-const ServerKeyPair = dotpCrypt.getKeyPair(ServerPrivateKey)
-const OTPCharSet = '023456789abcdefghjkmnpqrstuvwxyz'
-
 
 function randomUpTo(k, r, s) {
   let uint8 = new Uint8Array(1)
@@ -38,25 +33,22 @@ class Challenger extends React.Component {
 
   constructor (p, c) {
     super(p, c)
-    console.log(this.props)
     this.state = this.getInitialState()
-    console.log(this.state)
   }
 
   getInitialState() {
+    let randomSecret = new Uint8Array(32)
+    crypto.getRandomValues(randomSecret)
+    let serverKeyPair = dotpCrypt.getKeyPair(randomSecret)
     let recPublicId = this.props.routeParams.keyId
-    let nonce = new Uint8Array(24); window.crypto.getRandomValues(nonce);
-    let expiresAt = Math.floor(Date.now()/1000) + 360
-    let otp = this._getRandomOTP(10)
-    let challenge = dotpCrypt.createChallenge(new Buffer(otp), nonce, expiresAt, ServerKeyPair, recPublicId)
+    let otp = this._getRandomOTP(9)
+    let challenge = dotpCrypt.createChallenge(new Buffer(otp), serverKeyPair, recPublicId)
     return {
       recipient: recPublicId,
-      nonce: nonce,
-      expiresAt: expiresAt,
       challenge: challenge,
       otp: otp,
-      response: null,
-      serverKeyPair: ServerKeyPair,
+      response: '',
+      serverKeyPair: serverKeyPair,
       success: false,
     };
   }
@@ -67,22 +59,22 @@ class Challenger extends React.Component {
     let otp = ''
     for (let i=0; i < len; i++) {
       otp = otp + (randomUpTo(10))
-      //otp = otp + OTPCharSet.substr(randArr[i]%32,1)
     }
     console.log('OTP', otp)
     return otp
   }
 
-  _checkOTP(e) {
-    e.preventDefault()
-    if (this.state.otp === this.state.response) {
+  _checkOTP(val) {
+    if (this.state.otp === val.replace(/\s/g,'')) {
       this.setState({success: true})
+    } else {
+      this.setState({success: false})
     }
-    console.log(this.state)
   }
 
   _updateResponse(e) {
     this.setState({response: e.target.value})
+    this._checkOTP(e.target.value)
   }
 
   componentDidMount() {
@@ -102,20 +94,15 @@ class Challenger extends React.Component {
         <div className='container align-left'>
           <div className='row'>
             <div className='col-md-6 col-md-offset-3'>
-              <form onSubmit={this._checkOTP.bind(this)}>
                 <div className="form-group">
                   <label>One Time Password</label>
                   <input type="text" className={this.state.success ? "form-control success" :"form-control"} id="otp" placeholder="One Time Password" onChange={this._updateResponse.bind(this)} value={this.state.response} />
                 </div>
-                <button type="submit" className="btn btn-default">Check</button>
-              </form>
               <h4>Nitty Gritty</h4>
               <table className='table'>
                 <tbody>
                   <tr><td><strong>PublicKey:</strong></td><td>{this.state.recipient}</td></tr>
                   <tr><td><strong>Server Public Key: </strong></td><td>{dotpCrypt.utils.Base58.encode(this.state.serverKeyPair.publicKey)}</td></tr>
-                    <tr><td><strong>Nonce: </strong></td><td>{dotpCrypt.utils.Base58.encode(this.state.nonce)}</td></tr>
-                    <tr><td><strong>Expires At: </strong></td><td>{new Date(this.state.expiresAt * 1000).toString()}</td></tr>
                 </tbody>
               </table>
             </div>
